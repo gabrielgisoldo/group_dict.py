109b42eef82af8250f5efcb7440d2537e7057f14
gabriel diniz gisodlo <gabrielgisoldo@users.noreply.github.com>
2017-05-11 14:15:41 -0300

    Key error problem corrected

--- group_dict.py
+++ group_dict.py
@@ -2,35 +2,38 @@ def group_dict(self, group_by='', dict_list=[], filter_keys=[]):
         """
         Group the data from a dict.
 
-        This method receives a string representing the Dict Key it's going to
+        This method receives a value representing the Dict Key it's going to
         be used to group the data, the list of Dicts you want to work on and
         the list of Keys you want to keep after grouping everything.
         The param 'filter_keys' is optional, if you don't inform any key,
         it's going to keep the original Dict.
         """
-        if not isinstance(group_by, str) or len(group_by) == 0:
-            return 0
+        if not group_by:
+            return "No key to group by informed."
 
         if not isinstance(dict_list, list) or len(dict_list) == 0:
-            return 0
+            return "dict_list should be an list."
         else:
             for i in dict_list:
                 if not isinstance(i, dict) or len(i.keys()) == 0:
-                    return 0
+                    return "Every item inside the list(dict_list) should be a valid Dict."
 
         if not isinstance(filter_keys, list):
             return 0
 
-        return {
-            i: map(
-                lambda y: len(filter_keys) > 0 and {
-                    key: y[key] for key in filter_keys if key in y.keys()
-                } or y, filter(lambda x: x[group_by] == i, dict_list)
-            ) for i in list(
-                set(
-                    map(
-                        lambda x: x[group_by], dict_list
+            return {
+                i: map(
+                    lambda y: len(filter_keys) > 0 and {
+                        key: y[key] for key in filter_keys if key in y.keys()
+                    } or y, filter(lambda x: x[group_by] == i, dict_list)
+                ) for i in list(
+                    set(
+                        map(
+                            lambda x: x[group_by],
+                            filter(
+                                lambda k: group_by in k.keys(), dict_list
+                            )
+                        )
                     )
                 )
-            )
-        }
+            }
